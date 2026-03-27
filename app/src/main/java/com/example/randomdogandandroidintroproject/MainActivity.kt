e6b75051ad1e1508c71544259b09473e64a55f55
christian.winge <winge.chris@gmail.com>
2026-03-27 11:49:53 +0100

    UPG: reordered imports

--- app/src/main/java/com/example/randomdogandandroidintroproject/MainActivity.kt
+++ app/src/main/java/com/example/randomdogandandroidintroproject/MainActivity.kt
@@ -39,9 +39,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.randomdogandandroidintroproject.ui.theme.RandomDogAndAndroidIntroProjectTheme
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 
 // You can only use imports which is defined in gradle/libs.versions.toml and app/build.gradle.kts first.
 // Then Gradle handles it, by fetching the libs, and setting the dependency, so the libs can be used in code (write imports first)
